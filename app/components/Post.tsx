c8810dad0d4b7bab1ffb7430e33d3001479db7e3
Antonio Contreras <bahlone@gmail.com>
2023-09-08 10:15:11 -0700

    Data abstraction (#4)
    * Abstracting out data for posts and picks
    
    * Fixing TypeScript error on Home page JSX
    
    * Disabling page-previews for launch
    
    ---------
    
    Co-authored-by: Antonio Contreras <dev@richpicks.com>

---
+++ app/components/Post.tsx
@@ -0,0 +1,64 @@
+import { format } from 'date-fns'
+import { PickType, PostType } from '@/app/lib/types'
+import { getAllDone, getScoreboard } from '@/app/lib/utils'
+import { Pick } from '@/app/components/Pick'
+
+const Post = ({ data, heading }: {
+  data: PostType
+  heading: string
+}) => {
+  const { picksCollection: { items } } = data
+  const allDone = getAllDone(items)
+  const scoreboard = getScoreboard(items)
+  return (
+    <section className="section">
+      <div className="container">
+        <header className="header">
+          <time className="eyebrow" dateTime={data.date}>
+            {format(new Date(data.date), `EEEE 'the' do 'of' LLLL yyyy`)}
+          </time>
+          <h1 className="heading">
+            {heading}
+          </h1>
+          {allDone && (
+            <p className="summary">{scoreboard.wins} out of {items.length}</p>
+          )}
+        </header>
+        <ol className="games">
+          {items.map((item: PickType) => (
+            <li className="game" key={item.sys.id}><Pick data={item} /></li>
+          ))}
+        </ol>
+        {!allDone && (
+          <p className="graph">
+            Check out the latest odds at <a href="https://sportsbook.draftkings.com/leagues/football/nfl">DraftKings</a>, <a href="https://www.espn.com/nfl/lines">ESPN</a>, or <a href="https://sportsbook.fanduel.com/navigation/nfl">FanDuel</a>.
+          </p>
+        )}
+      </div>
+    </section>
+  )
+  // return (
+  //   <section>
+  //     <header>
+  //       <h1>{heading} -- {allDone ? 'dunzo' : 'not yet...'}</h1>
+  //       <p>{data.date}</p>
+  //     </header>
+  //     <div>
+  //       {items.map((item: PickType) => (
+  //         <Pick key={item.sys.id} data={item} />
+  //       ))}
+  //     </div>
+  //     {allDone && (
+  //       <footer>
+  //         <ul>
+  //           <li>wins: {scoreboard.wins}</li>
+  //           <li>losses: {scoreboard.losses}</li>
+  //           <li>ties: {scoreboard.ties}</li>
+  //         </ul>
+  //       </footer>
+  //     )}
+  //   </section>
+  // )
+}
+
+export { Post }
---
+++ app/lib/api.ts
@@ -0,0 +1,69 @@
+async function fetchGraphQL(query: string, preview = false) {
+  return fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${
+          preview
+            ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
+            : process.env.CONTENTFUL_ACCESS_TOKEN
+        }`,
+      },
+      body: JSON.stringify({ query }),
+    }
+  ).then((response) => response.json())
+}
+
+function extractMyEntries(fetchResponse: { data: { richPicksPostCollection: { items: any } } }) {
+  return fetchResponse?.data?.richPicksPostCollection?.items
+}
+
+export async function getItGirl(preview: boolean | undefined) {
+  const entries = await fetchGraphQL(
+    `query {
+      richPicksPostCollection(where: { archive_not: true }, limit: 18, order: date_DESC, preview: ${preview ? 'true' : 'false'}) {
+        items {
+          sys {
+            id
+          }
+          title
+          date
+          picksCollection(limit: 18, preview: ${preview ? 'true' : 'false'}) {
+            items {
+              sys {
+                id
+              }
+              title
+              bet
+              team {
+                name
+                location
+              }
+              game {
+                title
+                awayTeam {
+                  name
+                  location
+                }
+                awayScore
+                awaySpread
+                homeTeam {
+                  name
+                  location
+                }
+                homeScore
+                homeSpread
+                points
+                done
+              }
+            }
+          }
+        }
+      }
+    }`,
+    preview
+  )
+  return extractMyEntries(entries)
+}
