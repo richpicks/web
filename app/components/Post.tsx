630e82efa08e9c35300dfe21bb5de5a758cbaf01
Antonio Contreras <dev@richpicks.com>
2023-09-23 18:30:21 -0700

    Updating the odds-messaging for each post

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -14,31 +14,37 @@ const Post = ({ data }: {
   const totalPicks = items.length
   return (
     <section className={styles.section}>
+      <header className="container">
+        <time className={styles.eyebrow} dateTime={firstPublishedAt}>
+          {format(new Date(firstPublishedAt), `do 'of' LLLL yyyy`)}
+        </time>
+        <h1 className={styles.heading}>
+          {allDone ? `${title} ${getPostResults(wins, totalPicks)}` : 'This week Rich picks'}
+        </h1>
+        {allDone && (
+          <p className={styles.summary}>
+            {wins} out of {totalPicks}
+          </p>
+        )}
+      </header>
       <div className="container">
-        <header className={styles.header}>
-          <time className={styles.eyebrow} dateTime={firstPublishedAt}>
-            {format(new Date(firstPublishedAt), `do 'of' LLLL yyyy`)}
-          </time>
-          <h1 className={styles.heading}>
-            {allDone ? `${title} ${getPostResults(wins, totalPicks)}` : 'This week Rich picks'}
-          </h1>
-          {allDone && (
-            <p className={styles.summary}>
-              {wins} out of {totalPicks}
-            </p>
-          )}
-        </header>
         <ol className={styles.list}>
           {items.map((item: PickType) => (
             <li className={styles.item} key={item.sys.id}><Pick data={item} /></li>
           ))}
         </ol>
-        {!allDone && (
+      </div>
+      <footer className="container">
+        {allDone ? (
           <p className={styles.graph}>
-            Check out the latest odds at <a href="https://sportsbook.draftkings.com/leagues/football/nfl">DraftKings</a>, <a href="https://www.espn.com/nfl/lines">ESPN</a>, or <a href="https://sportsbook.fanduel.com/navigation/nfl">FanDuel</a>.
+            Odds courtesy of <a href="https://www.espn.com/nfl/lines">ESPN</a>.
+          </p>
+        ) : (
+          <p className={styles.graph}>
+            Odds reflect the opening lines. Please see <a href="https://www.espn.com/nfl/lines">ESPN</a> for the latest odds.
           </p>
         )}
-      </div>
+      </footer>
     </section>
   )
 }
