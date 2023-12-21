4b483d2f6c48d4183fe8850e870e25117493cf6e
Antonio Contreras <dev@richpicks.com>
2023-12-21 08:30:12 -0800

    Updating GraphQL query for scoreboard

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -4,10 +4,12 @@ import { getAllDone, getScoreboard, getPostResults } from '@/app/lib/utils'
 import { Pick } from '@/app/components/Pick'
 import styles from './Post.module.scss'
 
-const Post = ({ data }: {
-  data: PostType
-}) => {
-  const { picksCollection: { items }, sys: { firstPublishedAt }, title } = data
+const Post = ({ data }: { data: PostType }) => {
+  const {
+    picksCollection: { items },
+    sys: { firstPublishedAt },
+    title,
+  } = data
   const allDone = getAllDone(items)
   const scoreboard = getScoreboard(items)
   const { wins } = scoreboard
@@ -19,7 +21,9 @@ const Post = ({ data }: {
           {format(new Date(firstPublishedAt), `do 'of' LLLL yyyy`)}
         </time>
         <h1 className={styles.heading}>
-          {allDone ? `${title} ${getPostResults(wins, totalPicks)}` : 'This week Rich picks'}
+          {allDone
+            ? `Rich picked the ${title} ${getPostResults(wins, totalPicks)}`
+            : 'This week Rich picks'}
         </h1>
         {allDone && (
           <p className={styles.summary}>
@@ -30,18 +34,23 @@ const Post = ({ data }: {
       <div className="container">
         <ol className={styles.list}>
           {items.map((item: PickType) => (
-            <li className={styles.item} key={item.sys.id}><Pick data={item} /></li>
+            <li className={styles.item} key={item.sys.id}>
+              <Pick data={item} />
+            </li>
           ))}
         </ol>
       </div>
       <footer className="container">
         {allDone ? (
           <p className={styles.graph}>
-            Final scores and odds courtesy of <a href="https://www.espn.com/nfl/scoreboard">ESPN</a>.
+            Final scores and odds courtesy of{' '}
+            <a href="https://www.espn.com/nfl/scoreboard">ESPN</a>.
           </p>
         ) : (
           <p className={styles.graph}>
-            Odds reflect the opening lines. Please see <a href="https://www.espn.com/nfl/lines">ESPN</a> for the latest odds.
+            Odds reflect the opening lines. Please see{' '}
+            <a href="https://www.espn.com/nfl/lines">ESPN</a> for the latest
+            odds.
           </p>
         )}
       </footer>
